c0a4b04fec77abf2b2699289ed0b02bbe3c9b456
Philipp Benner <philipp.benner@bam.de>
2024-11-04 20:39:15 +0100

    Moved files from data_scripts directory

--- syncotrainmp/utility/crystal_funcs.py
+++ syncotrainmp/utility/crystal_funcs.py
@@ -1,39 +1,26 @@
+import numpy as np
 # This supresses warnings.
-from struct import Struct
 import warnings
 warnings.filterwarnings('ignore')
 
-import numpy as np
-import sys
-import logging
-import os
-import pandas as pd
 # from pymatgen.ext.matproj import MPRester  #legacy version
 from mp_api.client import MPRester
-import requests
-import json
-import pickle
-from typing import Iterable
+
 from pymatgen.analysis.structure_analyzer import OxideType
 from pymatgen.analysis.bond_valence import BVAnalyzer
-from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
-from pymatgen.analysis.chemenv.coordination_environments.chemenv_strategies import SimplestChemenvStrategy
-from pymatgen.analysis.chemenv.coordination_environments.coordination_geometry_finder import LocalGeometryFinder
-from pymatgen.analysis.chemenv.coordination_environments.structure_environments import LightStructureEnvironments
-from pymatgen.analysis.chemenv.utils.defs_utils import AdditionalConditions
-from pymatgen.analysis.chemenv.connectivity.connectivity_finder import ConnectivityFinder
-from pymatgen.util.coord import get_angle
 from pymatgen.core.structure import Structure
-from pymatgen.analysis.chemenv.connectivity.structure_connectivity import StructureConnectivity
 
-from typing import Generator, Iterable, Sequence, Any, Union, List, Optional, Tuple
+from typing import Union, List, Tuple
 
 
-def exper_oxygen_query(MPID : str, 
-                     theoretical_data = False, 
-                     num_sites = (1,150),
-                     fields : Union[str, List[str]] = "default",) -> str:
-    '''Retrieves experimental (ICSD) crystallographic data through the Materials Project API.
+def exper_oxygen_query(
+        MPID : str, 
+        theoretical_data = False, 
+        num_sites = (1,150),
+        fields : Union[str, List[str]] = "default",
+    ) -> str:
+    '''
+    Retrieves experimental (ICSD) crystallographic data through the Materials Project API.
     Currently queries for crystals which contain Oxide anions, are not theoretical, and have at least 2 elements.
     It stores their (pretty) formula, structure object and material_id.
     Parameters:
@@ -42,59 +29,57 @@ def exper_oxygen_query(MPID : str,
         The user ID from Materials Project which allows data query.
     Location : string
 	The directory in which the downloaded data will be stored.
-'''
+    '''
 
     if fields == "default":
-        fields = ["material_id", 
-                "formula_pretty", 
-                'composition',
-                "structure",
-                "e_total",
-                'elements',
-                'energy_above_hull',
-                'energy_per_atom',
-                'formation_energy_per_atom',
-                'is_stable',
-                'theoretical',
-                'deprecated',
-                ]
-    
+        fields = [
+            "material_id", 
+            "formula_pretty", 
+            'composition',
+            "structure",
+            "e_total",
+            'elements',
+            'energy_above_hull',
+            'energy_per_atom',
+            'formation_energy_per_atom',
+            'is_stable',
+            'theoretical',
+            'deprecated',
+        ]
+
     with MPRester(MPID) as mpr:
         db_version = mpr.get_database_version()
-    # If you get error 404, try upgrading mp-api from the terminal.
+        # If you get error 404, try upgrading mp-api from the terminal.
         results = mpr.summary.search(
             elements=["O"],
             num_elements = (2,100),
             theoretical=theoretical_data,
             num_sites = num_sites,  #change this to (1,150) when pipeline is ready.
-        #  all_fields=True,
+            #  all_fields=True,
             fields=fields
-                                    )
-        
+    )
+
     print("Database version is ", db_version)    
     print("The number of entries retrieved is ", len(results))
     results = [d.dict() for d in results]  #so it can be pickled/saved on disk.
     _ = [d.pop('fields_not_requested') for d in results]
     del _
-    
-        
+
     arrdata = np.array(results) #converts list to array, much faster to work with
-    del results #free up memory
-    
-    # if return_data:
-        # return arrdata
+    del results
+
     return arrdata, db_version
 
 
-
 def oxide_check(initStruc : Structure) -> Tuple[bool, bool, bool, Structure]:
-    '''Checks the oxidation states of the atoms in primary cell. Retruns the primary structure, and
-        booleans for whether the structure is bad, whether anions other than Oxygen are present and 
-        whether the oxidatoin number of Oxygen is anything other tha -2.
-        Parameters:
-        ----------------
-        initStruc : Structure
-            The Structure object as it was queried from MP database.
+    '''
+    Checks the oxidation states of the atoms in primary cell. Returns the primary structure, and
+    booleans for whether the structure is bad, whether anions other than Oxygen are present and 
+    whether the oxidatoin number of Oxygen is anything other tha -2.
+    Parameters:
+    ----------------
+    initStruc : Structure
+        The Structure object as it was queried from MP database.
     '''
 
     primStruc = initStruc  #just in case the conversion to primitive doesn't work, the type remains consistent when function returns    
@@ -128,9 +113,14 @@ def oxide_check(initStruc : Structure) -> Tuple[bool, bool, bool, Structure]:
 
 
 
-def clean_oxide(experimental : bool, pymatgenArray : np.ndarray, 
-                reportBadData : bool = False, read_oxide_type : bool = True) -> np.ndarray:
-    '''Filters undesired data points from the pymatgen data.
+def clean_oxide(
+        experimental : bool,
+        pymatgenArray : np.ndarray, 
+        reportBadData : bool = False,
+        read_oxide_type : bool = True
+    ) -> np.ndarray:
+    '''
+    Filters undesired data points from the pymatgen data.
     Undesired data here include: 1- structures which cannot be converted to primitive cell. 
     2- data the oxidation states of which cannot be analyzed. 
     3- Include any anion element which is not Oxygen. 
@@ -155,27 +145,27 @@ def clean_oxide(experimental : bool, pymatgenArray : np.ndarray,
 
     for j, material in enumerate(pymatgenArray):       
         if read_oxide_type:
-            if OxideType(material.structure).oxide_type != "oxide":
-                other_oxidation_IDs.append([j,material.material_id])
+            if OxideType(material["structure"]).oxide_type != "oxide":
+                other_oxidation_IDs.append([j,material['material_id']])
                 continue
-# read_oxide_type is not necessary, but it speeds up the filtering when available.            
+        # read_oxide_type is not necessary, but it speeds up the filtering when available.            
         if experimental:
-            if material.energy_above_hull>1:
-                ustable_experimental_IDs.append([j,material.material_id])
+            if material['energy_above_hull']>1:
+                ustable_experimental_IDs.append([j,material['material_id']])
                 continue
         try:
-            other_anion, other_oxidation, bad_structure, primStruc = oxide_check(initStruc=material.structure)
+            other_anion, other_oxidation, bad_structure, primStruc = oxide_check(initStruc=material["structure"])
             if other_anion:
-                other_anion_IDs.append([j,material.material_id])
+                other_anion_IDs.append([j,material['material_id']])
             elif other_oxidation:
-                other_oxidation_IDs.append([j,material.material_id])
+                other_oxidation_IDs.append([j,material['material_id']])
             elif bad_structure:
-                bad_structure_IDs.append([j,material.material_id])
+                bad_structure_IDs.append([j,material['material_id']])
             else: #this else only refers to the previous if
-                material.structure = primStruc #replaces the existing structure with the primitive structure
+                material["structure"] = primStruc #replaces the existing structure with the primitive structure
 
         except ValueError: 
-            valence_problem_IDs.append([j,material.material_id])
+            valence_problem_IDs.append([j,material['material_id']])
 
     print("The number of entries with anions other than Oxygen were", len(other_anion_IDs))
     print("The number of entries with different oxidation types were", len(other_oxidation_IDs))
@@ -201,4 +191,3 @@ def clean_oxide(experimental : bool, pymatgenArray : np.ndarray,
     bad_structure_IDs = [i[1] for i in bad_structure_IDs]
 
     return goodata, other_anion_IDs, ustable_experimental_IDs, other_oxidation_IDs, valence_problem_IDs, bad_structure_IDs
-
